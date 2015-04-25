fb9cc84b768b9b74290318b27078ffdc652e43e8
Art (giles) Hundiak <ahundiak@gmail.com>
2015-04-25 08:55:41 -0500

    Initial commit

--- test/test.js
+++ test/test.js
@@ -1,16 +1,22 @@
-var assert = require("assert");
+var assert = require('chai').assert;
+var expect = require('chai').expect;
 
 describe('Array', function(){
   describe('#indexOf()', function(){
     it('should return -1 when the value is not present', function(){
       assert.equal(-1, [1,2,3].indexOf(5));
       assert.equal(-1, [1,2,3].indexOf(0));
+
+      expect([1,2,3].indexOf(5)).to.equal(-1);
+      expect([1,2,3].indexOf(0)).to.equal(-1);
+      expect([1,2,3].indexOf(2)).to.equal( 1);
     });
   });
 
   describe('#length()', function(){
     it('should return 3', function(){
       assert.equal(3, [1,2,3].length);
+      expect([1,2,3]).to.have.length(3);
     });
   });
 });
