d1778d4366e446bffa2e145ea687a377811ab383
Art (giles) Hundiak <ahundiak@gmail.com>
2015-04-24 21:09:05 -0500

    Initial commit

---
+++ test/calc-test.js
@@ -0,0 +1,13 @@
+/**
+ * Created by ahundiak on 4/24/2015.
+ */
+var assert = require("assert");
+var calc = require('../src/calc');
+
+describe('Calculator', function() {
+  describe('#multiple()', function () {
+    it('should return 6 for 2x3', function () {
+      assert.equal(6, calc.multiply(2,3));
+    })
+  })
+});
